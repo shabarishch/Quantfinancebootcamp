693fa2a15f0c290ec2798334edbcd58dbac661bb
shabarishch <78301708+shabarishch@users.noreply.github.com>
2025-06-27 11:25:48 -0400

    Preliminary code for hedging in garch model

---
+++ functions_garch.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import json
+import datetime
+import os
+import time
+import pytz
+import matplotlib.pyplot as plt
+import scipy
+from arch import arch_model
+
+
